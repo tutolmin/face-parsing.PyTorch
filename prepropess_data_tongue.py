9510d0587c356f82f80d49ec25332a01988fb654
Andrei Tutolmin <tutolmin@gmail.com>
2025-09-11 17:25:27 +0300

    tongue masks

--- prepropess_data_tongue.py
+++ prepropess_data_tongue.py
@@ -7,16 +7,16 @@ import cv2
 from transform import *
 from PIL import Image
 
-face_sep_mask = '/home/andrei/data/CelebAMask-HQ/CelebAMask-HQ-mask-anno_orig'
-mask_path = '/home/andrei/data/CelebAMask-HQ/CelebAMask-HQ-mask_eye_g'
+face_sep_mask = '/home/andrei/data/job4/out/mask'
+mask_path = '/home/andrei/data/CelebAMask-HQ/CelebAMask-HQ-mask_tongue'
 counter = 0
 total = 0
-for i in range(15):
+for i in range(15, 16):
 
 #    atts = ['skin', 'l_brow', 'r_brow', 'l_eye', 'r_eye', 'eye_g', 'l_ear', 'r_ear', 'ear_r',
 #            'nose', 'mouth', 'u_lip', 'l_lip', 'neck', 'neck_l', 'cloth', 'hair', 'hat']
 
-    atts = ['eye_g']
+    atts = ['tongue']
 
     for j in range(i * 2000, (i + 1) * 2000):
 
@@ -27,6 +27,8 @@ for i in range(15):
             total += 1
             file_name = ''.join([str(j).rjust(5, '0'), '_', att, '.png'])
             path = osp.join(face_sep_mask, str(i), file_name)
+
+            print( path)
 
             if os.path.exists(path):
                 counter += 1
